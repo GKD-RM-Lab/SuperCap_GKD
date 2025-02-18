a27876272b16d93ff9b50efdb0df0d2fa978046b
ZY_Yu <hnsxcsxcxyzy@gmail.com>
2025-02-18 17:59:45 +0800

    new

---
+++ flash.sh
@@ -0,0 +1,4 @@
+make -j6 HARDWARE_ID=101
+# openocd -f interface/stlink.cfg -f target/stm32f3x.cfg -c "program build/RM2024-SuperCap-F3-V1R.bin 0x08000000 verify" -c "reset run" -c "exit"
+
+openocd -f interface/stlink.cfg -f target/stm32f3x.cfg -c "init" -c "halt" -c "program build/RM2024-SuperCap-F3-V1R.bin 0x08000000 verify" -c "reset run" -c "exit"
